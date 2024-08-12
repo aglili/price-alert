073252bddecfdccd4bdfd963b1ea9dda37711f8e
aglili <cecilselorm34@gmail.com>
2024-08-12 08:19:08 +0000

    changes 1

--- app/scheduler/daily.py
+++ app/scheduler/daily.py
@@ -1,9 +1,15 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 
+from app.api.database.config import get_db
+from app.api.utils.ebay_scrape import ParseEbayListing
 from app.scheduler.tasks.check_and_mail import check_all_listings_and_mail
 
 scheduler = BackgroundScheduler()
+
+
+db = next(get_db())
+parser = ParseEbayListing()
 
 
 scheduler.add_job(
@@ -11,4 +17,5 @@ scheduler.add_job(
     CronTrigger.from_crontab("0 7 * * *"),
     id="check_and_mail",
     replace_existing=True,
+    args=[db, parser],
 )
